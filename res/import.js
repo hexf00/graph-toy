2f60f119668077468d641a069a1e206ad21efc17
hexf00 <hexf00@gmail.com>
2020-01-20 18:01:11 +0800

    feat: 导入导出模块化

---
+++ res/import.js
@@ -0,0 +1,17 @@
+
+var importData = function (el) {
+
+    var file = el.files[0];
+
+    console.log(file.type);
+    if (!/application\/json/.test(file.type)) {
+        alert("文件格式错误！");
+        return false;
+    }
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function (e) {
+        //更新数据
+        graph.changeData(JSON.parse(this.result));
+    }
+}
