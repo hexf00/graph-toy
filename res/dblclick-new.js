7760a13902a7c93edea0cc526f17e3e2f21d8ba7
hexf00 <hexf00@gmail.com>
2020-01-25 23:18:33 +0800

    fix: 双击节点添加realId，双击修改的判断键

--- res/dblclick-new.js
+++ res/dblclick-new.js
@@ -12,7 +12,14 @@ G6.registerBehavior('dblclick-new', {
         const { item } = e;
         const graph = this.graph;
         var id = new Date().getTime() + parseInt(Math.random() * 1000).toString();
-        graph.addItem('node', { id: id, label: "新节点", x: e.x, y: e.y })
+
+        graph.addItem('node', {
+            id: id,
+            realId: id,
+            label: "新节点",
+            x: e.x,
+            y: e.y
+        })
 
         //addItem方法不会重新布局，应该要提供x,y信息
     },
