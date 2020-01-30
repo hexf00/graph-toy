75ba0e075ec84917067eb497d4ed5d768ab32431
hexf00 <hexf00@gmail.com>
2020-01-30 19:02:10 +0800

    feat: 恢复元素多选

--- res/node-click.js
+++ res/node-click.js
@@ -12,16 +12,13 @@ G6.registerBehavior('node-click', {
         const { item } = e;
         const graph = this.graph;
 
-        console.log(e);
 
-
-        // if (!ControlStatus) {
-            //关闭多选
+        if (!ControlStatus) {
             const selectedNodes = graph.findAllByState('node', 'selected');
             selectedNodes.forEach(cn => {
                 graph.setItemState(cn, 'selected', false);
             });
-        // }
+        }
 
         graph.setItemState(item, 'selected', true);
 
