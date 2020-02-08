5207c5d854df366439a5d6b8bd83a74f586f1a90
hexf00 <hexf00@gmail.com>
2020-02-08 22:19:17 +0800

    feat: 数据规范检测函数-重复边检测

--- res/data-check.js
+++ res/data-check.js
@@ -1,26 +1,60 @@
 
 //数据检查
 
-//检查边重复
+
 
 
 //检查自连接
-function dumpSelfEdges(){
+function dumpSelfEdges() {
     var data = graph.save();
-    var selfEdges = data.edges.filter((edge)=>{
-        let {source, target} = edge;
+    var selfEdges = data.edges.filter((edge) => {
+        let { source, target } = edge;
         return source == target;
     });
 
     console.table(selfEdges);
 }
 //删除自连接
-function deleteSelfEdges(){
+function deleteSelfEdges() {
     var data = graph.save();
-    data.edges = data.edges.filter((edge, index)=>{
-        let {source, target} = edge;
+    data.edges = data.edges.filter((edge, index) => {
+        let { source, target } = edge;
         return source !== target;
     });
 
     graph.changeData(data);
+}
+
+//检查边重复
+function dumpRepeatEdges() {
+    var data = graph.save();
+
+    var repeatEdgesIndex = [];
+
+    for (let edgeAIndex = 0; edgeAIndex < data.edges.length; edgeAIndex++) {
+        const edgeA = data.edges[edgeAIndex];
+
+        if (repeatEdgesIndex.indexOf(edgeAIndex) !== -1) {
+            continue;
+        }
+
+        var repeatEdges = data.edges.filter((edgeB, edgeBIndex) => {
+            // if (edgeAIndex == edgeBIndex) {
+            //     return false;
+            // }
+            let { source, target } = edgeB;
+            if (source == edgeA.source && target == edgeA.target) {
+                repeatEdgesIndex.push(edgeBIndex)
+                return true;
+            }
+        });
+
+        if (repeatEdges.length > 1) {
+            //重复边
+            console.log(repeatEdges);
+        }
+
+    }
+
+
 }
