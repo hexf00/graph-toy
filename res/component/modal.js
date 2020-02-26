2f4ed9b8df73534a31d539a0658d62f6c907a2bd
hexf00 <hexf00@gmail.com>
2020-02-26 23:44:13 +0800

    feat: 添加modal框组件

---
+++ res/component/modal.js
@@ -0,0 +1,34 @@
+Vue.component('modal', {
+  template: /*html*/`
+    <div v-if="show" class="modal-bg">
+      <div class="modal">
+        <div class="modal-title">
+          <span>{{title}}</span>
+          <a class="close" @click="close">×</a>
+        </div>
+        <div class="modal-content">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  `,
+  props: {
+  },
+  computed: {
+  },
+  data() {
+    return {
+      title: "",
+      show: false
+    }
+  },
+  methods: {
+    open(title) {
+      this.title = title ? title : ""
+      this.show = true
+    },
+    close() {
+      this.show = false
+    }
+  },
+})  
