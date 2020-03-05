02f6e5ec70644885bcb90e2d689ff420b0d991c4
hexf00 <hexf00@gmail.com>
2020-03-06 01:34:23 +0800

    feat: 特征定义添加节点选择器

--- res/component/panel/characteristic-config-form.js
+++ res/component/panel/characteristic-config-form.js
@@ -1,7 +1,10 @@
 Vue.component('characteristic-config-form', {
   template: /*html*/`<div>
+      <modal v-if="editItem" ref="scriptEditor" @done="update">
+        <textarea v-model="editItem.filterScript" style="width:100%;height:150px" type="text"></textarea>
+      </modal>
       <table v-if="list.length > 0">
-        <tr><td>特征名称</td><td>特征输入控件</td></tr>
+        <tr><td>特征名称</td><td>特征值输入控件</td></tr>
         <tr v-for="item,i in list">
           <td>
             <input type="text" v-model="item.name" @input="submit">
@@ -9,7 +12,10 @@ Vue.component('characteristic-config-form', {
           <td>
             <select v-model="item.control.type">
               <option value="text" @input="submit">文本框</option>
+              <option value="node" @input="submit">节点选择器</option>
             </select>
+            
+            <a v-if="item.control.type == 'node'" @click="showScriptEditor(item)" href="javascript:;">配置</a>
             <a @click="remove(i)" href="javascript:;">删除</a>
           </td>
         </tr>
@@ -32,9 +38,20 @@ Vue.component('characteristic-config-form', {
   data() {
     return {
       list: this.value,
+      editItemRaw: null,
+      editItem: null,
     }
   },
   methods: {
+    update() {
+      this.editItemRaw.filterScript = this.editItem.filterScript
+      this.$refs.scriptEditor.close()
+    },
+    showScriptEditor(item) {
+      this.editItemRaw = item
+      this.editItem = JSON.parse(JSON.stringify(item))
+      this.$nextTick(() => this.$refs.scriptEditor.open("编辑过滤脚本配置"))
+    },
     remove(index) {
       this.list.splice(index, 1)
       this.submit()
